6094faf2af6593db5ea9f16f2abfac6ceff74f8f
mbell9 <mbell17bay@gmail.com>
2023-04-30 16:52:58 -0400

    datastorage plan

--- src/main/java/com/budget/budgettracking/DataStorage.java
+++ src/main/java/com/budget/budgettracking/DataStorage.java
@@ -1,4 +1,11 @@
 package com.budget.budgettracking;
 
 public class DataStorage {
+    // want to store in CSV
+    // want to store (username?, total budget, budgetList, transactionList)
+
+    // createAccount
+    // loadExistingUser?
+    // readFromCSV
+    // writeToCSV
 }
